c0125b095b14478d71975f3140437f8d0ffd4a90
paulinedavid <pauline.david@efrei.net>
2024-05-26 00:06:01 +0200

    random spawn + mine

--- DigOrDie/Assets/Scripts/Compass.cs
+++ DigOrDie/Assets/Scripts/Compass.cs
@@ -18,8 +18,11 @@ public class Compass : MonoBehaviour
     public Transform objective2;
     public Transform treasure;
 
+    public Transform mine;
+
     void Start()
     {
+        RandomSpawn();
         missionplace = objective1;
     }
 
@@ -37,6 +40,53 @@ public class Compass : MonoBehaviour
             else if (missionplace == objective2)
             {
                 missionplace = treasure;
+            }
+        }
+        if (Math.Abs(transform.position.x - mine.position.x) <= 2 && Math.Abs(transform.position.z - mine.position.z) <= 2)
+        {
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void RandomSpawn()
+    {
+        bool pasmaison = false;
+        while (!pasmaison)
+        {
+            objective1.position = new Vector3(UnityEngine.Random.Range(5, 275), 0, UnityEngine.Random.Range(5, 275));
+            if(objective1.position.x > 100 && objective1.position.x < 120 && objective1.position.z > 100 && objective1.position.z< 120)
+            {
+                pasmaison = false;
+            }
+            else
+            {
+                pasmaison = true;
+            }
+        }
+        pasmaison = false;
+        while (!pasmaison)
+        {
+            objective2.position = new Vector3(UnityEngine.Random.Range(5, 275), 0, UnityEngine.Random.Range(5, 275));
+            if (objective2.position.x > 100 && objective2.position.x < 120 && objective2.position.z > 100 && objective2.position.z < 120)
+            {
+                pasmaison = false;
+            }
+            else
+            {
+                pasmaison = true;
+            }
+        }
+        pasmaison = false;
+        while (!pasmaison)
+        {
+            treasure.position = new Vector3(UnityEngine.Random.Range(5, 275), 0, UnityEngine.Random.Range(5, 275));
+            if (treasure.position.x > 100 && treasure.position.x < 120 && treasure.position.z > 100 && treasure.position.z < 120)
+            {
+                pasmaison = false;
+            }
+            else
+            {
+                pasmaison = true;
             }
         }
     }
